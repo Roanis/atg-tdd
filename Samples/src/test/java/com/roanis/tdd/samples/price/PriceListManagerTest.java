741987362f337f41ec00f05eff4f7b6f26589309
Rory Curtis <rory_curtis@roanis.com>
2014-01-21 19:07:50 +0000

    Base prices complete.

--- Samples/src/test/java/com/roanis/tdd/samples/price/PriceListManagerTest.java
+++ Samples/src/test/java/com/roanis/tdd/samples/price/PriceListManagerTest.java
@@ -24,12 +24,18 @@ public class PriceListManagerTest extends BasePriceListTest {
 	}
 	
 	@Test
-	public void skuPriceExists() throws PriceListException {
+	public void skuListPriceExists() throws PriceListException {
 		RepositoryItem price = getPriceListManager().getPrice(getListPrices().getRepositoryId(), CatalogTestConstants.MENS_BELT_PRODUCT_ID, CatalogTestConstants.CASUAL_BELT_BROWN_SMALL_SKU_ID);
 		assertNotNull(price);
 		assertEquals(19.99D, price.getPropertyValue(getPriceListManager().getListPricePropertyName()));
 	}
 	
+	@Test
+	public void skuSalePriceExists() throws PriceListException{
+		RepositoryItem price = getPriceListManager().getPrice(getSalePrices().getRepositoryId(), CatalogTestConstants.PHONE_PRODUCT_ID, CatalogTestConstants.PHONE_SKU_ID);
+		assertNotNull(price);
+		assertEquals(269.00D, price.getPropertyValue(getPriceListManager().getListPricePropertyName()));
+	}
 	
 
 }
