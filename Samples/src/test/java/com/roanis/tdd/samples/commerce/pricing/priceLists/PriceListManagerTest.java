909b842dbedf00da529398ac84dfd49c90b2d778
Rory Curtis <rory_curtis@roanis.com>
2014-03-24 12:46:04 +0000

    Changed PriceList tests to use NucleusAwareJUnit4ClassRunner.

--- Samples/src/test/java/com/roanis/tdd/samples/commerce/pricing/priceLists/PriceListManagerTest.java
+++ Samples/src/test/java/com/roanis/tdd/samples/commerce/pricing/priceLists/PriceListManagerTest.java
@@ -1,40 +1,73 @@
 package com.roanis.tdd.samples.commerce.pricing.priceLists;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import atg.commerce.pricing.priceLists.PriceListException;
+import atg.commerce.pricing.priceLists.PriceListManager;
 import atg.repository.RepositoryItem;
 
 import com.roanis.tdd.core.commerce.catalog.CatalogTestConstants;
-import com.roanis.tdd.core.commerce.pricing.priceLists.BasePriceListTest;
+import com.roanis.tdd.core.commerce.pricing.priceLists.PriceListTestHelper;
+import com.roanis.tdd.junit4.runner.NucleusAwareJunit4ClassRunner;
+import com.roanis.tdd.util.TestUtils;
 
-@RunWith(JUnit4.class)
-public class PriceListManagerTest extends BasePriceListTest {
+@RunWith(NucleusAwareJunit4ClassRunner.class)
+public class PriceListManagerTest {
+	
+	private PriceListManager mPriceListManager;	
+	private PriceListTestHelper mPriceListTestHelper;
+	private RepositoryItem mListPrices;
+	private RepositoryItem mSalePrices;	
+	
+	@Before
+	public void setUp() throws Exception {	
+		mPriceListTestHelper = TestUtils.getTestConfiguration().getPriceListTestHelper();
+		mPriceListManager = mPriceListTestHelper.getPriceListManager();		
+		
+		setupDefaultPriceLists();		
+	}	
+	
+	@After
+	public void tearDown() throws Exception {
+		mPriceListTestHelper = null;
+		mPriceListManager = null;
+		mListPrices = null;
+		mSalePrices = null;				
+	}
+
+	protected void setupDefaultPriceLists() throws PriceListException {
+		mListPrices = mPriceListManager.getPriceList(mPriceListTestHelper.getDefaultPriceListId());
+		mSalePrices = mPriceListManager.getPriceList(mPriceListTestHelper.getDefaultSalePriceListId());
+	}
 		
 	@Test
 	public void priceListExists(){
-		assertNotNull(getListPrices());
+		assertNotNull(mListPrices);
 	}
 	
 	@Test
 	public void salePriceListExists(){
-		assertNotNull(getSalePrices());
+		assertNotNull(mSalePrices);
 	}
 	
 	@Test
 	public void skuListPriceExists() throws PriceListException {
-		RepositoryItem price = getPriceListManager().getPrice(getListPrices().getRepositoryId(), CatalogTestConstants.MENS_BELT_PRODUCT_ID, CatalogTestConstants.CASUAL_BELT_BROWN_SMALL_SKU_ID);
+		RepositoryItem price = mPriceListManager.getPrice(mListPrices.getRepositoryId(), CatalogTestConstants.MENS_BELT_PRODUCT_ID, CatalogTestConstants.CASUAL_BELT_BROWN_SMALL_SKU_ID);
 		assertNotNull(price);
-		assertEquals(19.99D, price.getPropertyValue(getPriceListManager().getListPricePropertyName()));
+		assertEquals(19.99D, price.getPropertyValue(mPriceListManager.getListPricePropertyName()));
 	}
 	
 	@Test
 	public void skuSalePriceExists() throws PriceListException{
-		RepositoryItem price = getPriceListManager().getPrice(getSalePrices().getRepositoryId(), CatalogTestConstants.PHONE_PRODUCT_ID, CatalogTestConstants.PHONE_SKU_ID);
+		RepositoryItem price = mPriceListManager.getPrice(mSalePrices.getRepositoryId(), CatalogTestConstants.PHONE_PRODUCT_ID, CatalogTestConstants.PHONE_SKU_ID);
 		assertNotNull(price);
-		assertEquals(269.00D, price.getPropertyValue(getPriceListManager().getListPricePropertyName()));
+		assertEquals(269.00D, price.getPropertyValue(mPriceListManager.getListPricePropertyName()));
 	}
 	
 
