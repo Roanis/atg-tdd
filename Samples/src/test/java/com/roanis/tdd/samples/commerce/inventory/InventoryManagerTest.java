f1099e8c6f371dd80e77c4700498d815ccc14553
Rory Curtis <rory_curtis@roanis.com>
2014-03-24 12:57:40 +0000

    InventoryManagerTest no longer needs a base class.

--- Samples/src/test/java/com/roanis/tdd/samples/commerce/inventory/InventoryManagerTest.java
+++ Samples/src/test/java/com/roanis/tdd/samples/commerce/inventory/InventoryManagerTest.java
@@ -1,21 +1,36 @@
 package com.roanis.tdd.samples.commerce.inventory;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import atg.commerce.inventory.InventoryException;
 import atg.commerce.inventory.InventoryManager;
 
 import com.roanis.tdd.core.commerce.catalog.CatalogTestConstants;
-import com.roanis.tdd.core.commerce.inventory.BaseInventoryTest;
+import com.roanis.tdd.junit4.runner.NucleusAwareJunit4ClassRunner;
+import com.roanis.tdd.util.TestUtils;
 
-@RunWith(JUnit4.class)
-public class InventoryManagerTest extends BaseInventoryTest {
+@RunWith(NucleusAwareJunit4ClassRunner.class)
+public class InventoryManagerTest {
+	private InventoryManager mInventoryManager;
+	
+	@Before
+	public void setUp() throws Exception {	
+		mInventoryManager = TestUtils.getTestConfiguration().getInventoryTestHelper().getInventoryManager();
+	}
+	
+	@After
+	public void tearDown(){
+		mInventoryManager = null;
+	}
 	
 	@Test
 	public void stockAvailable() throws InventoryException {
-		assertEquals(InventoryManager.AVAILABILITY_STATUS_IN_STOCK, getInventoryManager().queryAvailabilityStatus(CatalogTestConstants.BOOTS_SIZE_5_SKU_ID));				
+		assertEquals(InventoryManager.AVAILABILITY_STATUS_IN_STOCK, mInventoryManager.queryAvailabilityStatus(CatalogTestConstants.BOOTS_SIZE_5_SKU_ID));				
 	}
 
 }
